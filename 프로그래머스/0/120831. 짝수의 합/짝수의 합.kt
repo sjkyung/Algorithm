4cc4b7a9500a769d47aaaeb845f7a5e82e5c7c4d
Jaekyung Sim <97489787+sjkyung@users.noreply.github.com>
2024-02-28 00:29:14 +0900

    [level 0] Title: 짝수의 합, Time: 0.01 ms, Memory: 61.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun solution(n: Int): Int {
+        var answer: Int = 0
+        for(i in 1..n){
+            if(i % 2 == 0){
+                answer += i
+            }
+        }
+        
+        
+        return answer
+    }
+}
