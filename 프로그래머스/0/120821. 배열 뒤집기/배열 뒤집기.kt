c06a68552271195d828408090c4fb07b6477b785
Jaekyung Sim <97489787+sjkyung@users.noreply.github.com>
2024-03-03 22:53:55 +0900

    [level 0] Title: 배열 뒤집기, Time: 13.96 ms, Memory: 62.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.kt
@@ -0,0 +1,7 @@
+class Solution {
+    fun solution(num_list: IntArray): IntArray {
+        var answer: IntArray = intArrayOf()
+        answer = num_list.reversedArray()
+        return answer
+    }
+}
