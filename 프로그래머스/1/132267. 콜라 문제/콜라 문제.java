ead4496377c5c53260a1c69f81eff14a9051284b
Jaekyung Sim <97489787+sjkyung@users.noreply.github.com>
2024-03-11 23:18:53 +0900

    [level 1] Title: 콜라 문제, Time: 0.02 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int a, int b, int n) {
+        int count = 0;
+        int num = n; 
+        
+        while(num >= a){
+            count += num / a * b;
+            num = num / a * b + num % a;
+        
+        }
+        return count;
+        
+    }
+}
